9e0e8c917d25d5a94bf9ec039e093344de63e12a
Jennifer <jennbee@Jennifers-MacBook-Pro.local>
2016-08-01 16:17:09 -0400

    updated to most recent version of WP

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -35,7 +35,7 @@ get_header(); ?>
                     $size = "medium";
                 ?>
                 <li class="individual-featured-work">
-                    <figure>
+                    <figure class="case-study-thumbnails">
                         <a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image($image_1, $size); ?></a>
                     </figure>
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
