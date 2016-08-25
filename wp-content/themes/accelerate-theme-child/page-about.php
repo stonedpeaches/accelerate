03fad8593aef7d403ed5493396009796d8dfda66
jennbee <jennifer@justagirl.com>
2016-08-24 21:47:34 -0400

    float float float your boat

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -10,13 +10,13 @@
 get_header(); ?>
 
 <section class="aboutpage">
-	<div class="site-content">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="aboutpage-hero">
+    <div class="site-content">
+		  <?php while ( have_posts() ) : the_post(); ?>
+			 <div class="aboutpage-hero">
 				<?php the_content(); ?>
-			</div>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- site-content -->
+			 </div>
+		  <?php endwhile; // end of the loop. ?>
+    </div><!-- site-content -->
 </section><!-- aboutpage -->
 
 <div class="all-services">
@@ -28,40 +28,45 @@ get_header(); ?>
     );
     $services_query = new WP_Query($args);
 ?>
+    
+    <section class="our-services">
+        <h6>Our Services</h6>
+        <p>We take pride in our clients and the content we create for them.<br> Here's a brief overview of our offered services.</p>
+    </section>
 
-<?php while ($services_query-> have_posts() ) : $services_query->the_post();
-// Variables from 'services' CPT 
-    $service_description = get_field('service_description'); 
-    $service_image = get_field('service_image'); 
-    $size = "thumbnail";
-?>
 
-<div id="primary" class="site-content">
-    <div id="content" role="main">
+    <?php while ($services_query-> have_posts() ) : $services_query->the_post();
+    // Variables from 'services' CPT 
+        $service_description = get_field('service_description'); 
+        $service_image = get_field('service_image'); 
+        $size = "thumbnail";
+    ?>
 
-        <section class="service-section">
-            <figure class="service-image-left">
-                <?php echo wp_get_attachment_image( $service_image, $size ); ?>
-            </figure>
-            <div class="service-description">
-                <h3><?php the_title(); ?></h3>
-                <p><?php echo $service_description; ?></p>
-            </div>
-        </section>
+
+    <div id="primary" class="site-content">
+        <div id="content" role="main">
+
+            <section class="service-section">
+                <figure class="service-image">
+                    <?php echo wp_get_attachment_image( $service_image, $size ); ?>
+                </figure>
+                <div class="service-description">
+                    <h3><?php the_title(); ?></h3>
+                    <p><?php echo $service_description; ?></p>
+                </div>
+            </section>
         
-            
+        </div>
     </div>
-</div>
 				
-<?php endwhile; //end the while loop
-    wp_reset_postdata(); ?>
+    <?php endwhile; //end the while loop
+        wp_reset_postdata(); ?>
     
         <div class="about-contact-us">
 			<p>Interested in working with us?&nbsp;&nbsp;&nbsp;
-                <a class="button" href="<?php echo home_url(); ?>/contact">Contact Us</a></p>
+            <a class="button" href="<?php echo home_url(); ?>/contact">Contact Us</a></p>
         </div>
-
-				
+			
 </div><!-- .all-services -->
 
 <?php get_footer(); ?>
